f81364b372ad3945898cdb74e52ef64e3f56d69f
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-02-24 03:01:49 +0300

    vue

---
+++ static/final032/js/scriptForVue.js
@@ -0,0 +1,29 @@
+const funcs = new Vue({
+    el: '#revrse',
+    methods: {
+        rever() {
+            if (!this.$el.style.transform) {
+                this.$el.style.transform = 'scale(-1, 1)';
+            } else {
+                this.$el.style.transform = '';
+            }
+        }
+    }
+});
+
+const fncs = new Vue({
+    el: '#elem',
+    methods: {
+        ddTo(){
+            const li = document.createElement('li');
+            li.classList.add('btn', 'btn__color');
+            li.textContent = 'Новый элемент';
+            this.$el.closest('ul').appendChild(li);
+        }
+    }
+});
+
+const fff = new Vue({
+
+})
+
